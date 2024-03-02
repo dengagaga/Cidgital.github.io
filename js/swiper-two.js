b0e9107456ef27088b2762c9a1209f55de64f08f
dengagaga <134398955+dengagaga@users.noreply.github.com>
2024-03-02 19:16:53 +0300

    Add files via upload

--- js/swiper-two.js
+++ js/swiper-two.js
@@ -1,12 +1,40 @@
 var swiper = new Swiper(".mySwiper", {
-  slidesPerView: 2.2,
+  slidesPerView: 1,
   spaceBetween: 24,
     navigation: {
       nextEl: ".swiper-button-nexts",
       prevEl: ".swiper-button-prevs",
     },
+    breakpoints: {
+      310: {
+          slidesPerView: 1,
+      },
+      640: {
+          slidesPerView: 1,
+      },
+      768: {
+          slidesPerView: 2,
+      },
+      1240: {
+          slidesPerView: 2.2,
+      },
+  },
   });
 var swipers = new Swiper(".mySwiper2", {
-  slidesPerView: 2,
+  slidesPerView: 1,
   spaceBetween: 24,
+  breakpoints: {
+    310: {
+        slidesPerView: 1,
+    },
+    640: {
+        slidesPerView: 1,
+    },
+    768: {
+        slidesPerView: 2,
+    },
+    1240: {
+        slidesPerView: 3.1,
+    },
+},
   });
